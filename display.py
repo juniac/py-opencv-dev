a7f0b4f4801ac43d16618a398ce699c437ae2a56
Jeong-Un, Kim <juniac79@gmail.com>
2020-04-27 00:24:26 +0900

    Implement display

---
+++ display.py
@@ -0,0 +1,23 @@
+import sdl2
+import sdl2.ext
+
+class Display(object):
+    def __init__(self, width, height):
+        sdl2.ext.init()
+        self.width, self.height = width, height
+        self.window = sdl2.ext.Window("Py-OPENCV", size=(width, height), position=(0, 500))
+        self.window.show()
+
+
+    def paint(self, image):
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                exit(0)
+
+        # print(dir(window))
+        # surface = sdl2.ext.pixels2d(self.window.get_surface())
+        surface = sdl2.ext.pixels3d(self.window.get_surface())
+        surface[:, :, 0:3] = image.swapaxes(0, 1)
+        
+        self.window.refresh()
--- main.py
+++ main.py
@@ -2,45 +2,27 @@
 
 import cv2
 import time
+from display import Display
 # import sys
-import sdl2
-import sdl2.ext
+width = 3840//4
+height = 2160//4
 
-
-videoWidth = 3840//4
-videoHeight = 2160//4
-
-
-sdl2.ext.init()
-
+display = Display(width, height)
 
 def process_frame(image):
     image = cv2.resize(image, (3840//4, 2160//4))
-    events = sdl2.ext.get_events()
-    for event in events:
-        if event.type == sdl2.SDL_QUIT:
-            exit(0)
-
-    # print(dir(window))
-
-
-    cv2.imshow('image', image)
-
+    display.paint(image)
+    # cv2.imshow('image', image)
     print(image)
     print(image.shape)
 
-
 if __name__ == "__main__":
-
     cap = cv2.VideoCapture('videos/scotland.mov')
-
     if (cap.isOpened() == False):
         print("Error opening video")
-
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
             process_frame(frame)
         else:
-            break;
-
+            break
