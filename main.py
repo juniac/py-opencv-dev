f0851b038a4963ce65a22c1abdad0e7ee2b10720
Jeong-Un, Kim <juniac79@gmail.com>
2020-04-27 00:37:06 +0900

    Implement detecting ORB

--- main.py
+++ main.py
@@ -8,16 +8,27 @@ width = 3840//4
 height = 2160//4
 
 display = Display(width, height)
+orb = cv2.ORB_create()
+
 
 def process_frame(image):
     image = cv2.resize(image, (3840//4, 2160//4))
+
+    keyPoints, destination = orb.detectAndCompute(image, None)
+
+    for point in keyPoints:
+        # print(point.pt)
+        x, y = map(lambda x: int(round(x)), point.pt)
+        cv2.circle(image, (x,y), color=(0, 255, 0), radius=3)
+        # print([x, y])
+
     display.paint(image)
     # cv2.imshow('image', image)
-    print(image)
-    print(image.shape)
+    # print(image)
+    # print(image.shape)
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture('videos/scotland.mov')
+    cap = cv2.VideoCapture('videos/kyushu.mov')
     if (cap.isOpened() == False):
         print("Error opening video")
     while(cap.isOpened()):
