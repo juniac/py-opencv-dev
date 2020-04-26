0adeb7f182f1bde6e50d37157e7f708902fd7319
Jeong-Un, Kim <juniac79@gmail.com>
2020-04-27 00:08:48 +0900

    make video playable

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 #!/usr/bin/env python3
 
 import cv2
+import time
+# import sys
+import sdl2
+import sdl2.ext
 
-print('Hello')
+
+videoWidth = 3840//4
+videoHeight = 2160//4
+
+
+sdl2.ext.init()
+
+
+def process_frame(image):
+    image = cv2.resize(image, (3840//4, 2160//4))
+    events = sdl2.ext.get_events()
+    for event in events:
+        if event.type == sdl2.SDL_QUIT:
+            exit(0)
+
+    # print(dir(window))
+
+
+    cv2.imshow('image', image)
+
+    print(image)
+    print(image.shape)
+
+
+if __name__ == "__main__":
+
+    cap = cv2.VideoCapture('videos/scotland.mov')
+
+    if (cap.isOpened() == False):
+        print("Error opening video")
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            process_frame(frame)
+        else:
+            break;
+
