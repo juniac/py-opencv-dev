2c15d88416b1299f07f8dfe6efb99dcff7188645
Jeong-Un, Kim <juniac79@gmail.com>
2020-04-27 01:15:42 +0900

    Improve extractor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import cv2
 import time
+import numpy as np
 from display import Display
 # import sys
 width = 3840//4
@@ -10,18 +11,51 @@ height = 2160//4
 display = Display(width, height)
 orb = cv2.ORB_create()
 
+class FeatureExtractor(object):
+    GX = 16 // 2
+    GY = 12 // 2
+    
+    def __init__(self, width, height):
+        self.orb = cv2.ORB_create(1000)
+        self.width, self.height = width, height
+
+    def extract(self, image):
+       features = cv2.goodFeaturesToTrack(np.mean(image, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
+       print(features)
+       return features
+       """  
+        sy = image.shape[0] // self.GY
+        sx = image.shape[1] // self.GX
+        # print([image.shape[0], image.shape[1]])
+        a_key_points = []
+        for ry in range(0, image.shape[0], sy):
+            for rx in range(0, image.shape[1], sx):
+                image_chunk = image[ry:ry + sy, rx:rx + sx]
+                # print(image_chunk)
+                key_points = self.orb.detect(image_chunk, None)
+                print(key_points)
+                for point in key_points:
+                    point.pt = (point.pt[0] + rx, point.pt[1] + ry)
+                    a_key_points.append(point)
+                    # print(point.pt)
+                    # print(point)
+        return a_key_points 
+        """
+                
+
+feature_extractor = FeatureExtractor(width, height)
 
 def process_frame(image):
     image = cv2.resize(image, (3840//4, 2160//4))
+    key_points = feature_extractor.extract(image)
 
-    keyPoints, destination = orb.detectAndCompute(image, None)
-
-    for point in keyPoints:
-        # print(point.pt)
-        x, y = map(lambda x: int(round(x)), point.pt)
+    for f in key_points:
+        x, y = map(lambda x: int(round(x)), f[0])
         cv2.circle(image, (x,y), color=(0, 255, 0), radius=3)
+    # for point in key_points:
+    #     x, y = map(lambda x: int(round(x)), point.pt)
+    #     cv2.circle(image, (x,y), color=(0, 255, 0), radius=3)
         # print([x, y])
-
     display.paint(image)
     # cv2.imshow('image', image)
     # print(image)
