d41af372b25277e108b2baed40df96357196f821
Jeong-Un, Kim <juniac79@gmail.com>
2020-04-28 00:36:13 +0900

    nomalize coords

--- extractor.py
+++ extractor.py
@@ -3,12 +3,27 @@ import numpy as np
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 
-class FeatureExtractor(object):
+# turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+class Extractor(object):
 
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create(100)
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+        # self.width, self.height = width, height
+
+    def normalize(self, points):
+        return np.dot(self.Kinv, add_ones(points).T).T[:, 0:2]
+
+    def denormalize(self, point):
+        ret = np.dot(self.K, np.array([point[0], point[1], 1.0]))
+        return int(round(ret[0])), int(round(ret[1]))
+
+        # return int(round(point[0] + self.width)), int(round(point[1] + self.height))
 
     def extract(self, image):
         # detection
@@ -28,15 +43,26 @@ class FeatureExtractor(object):
                     key_point2 = self.last['key_points'][m.trainIdx].pt
                     ret.append((key_point1, key_point2))
         #    matches = zip([key_points[m.queryIdx] for m in matches], [self.last['key_points'][m.trainIdx] for m in matches])
+        
+        #normalize coors subtract to move to 0
+
+
         # filter
         if len(ret) > 0:
             ret = np.array(ret)
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
+            # ret[:, :, 0] -= image.shape[0]//2
+            # ret[:, :, 1] -= image.shape[1]//2
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
                                     FundamentalMatrixTransform,
                                     min_samples=8,
                                     residual_threshold=1,
                                     max_trials=100)
             ret = ret[inliers]
+            # s, v, d = np.linalg.svd(model.params)
+
 
         self.last = {'key_points': key_points, 'descriptors': descriptors}
 
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import cv2
 import time
 import numpy as np
 from display import Display
-from extractor import FeatureExtractor
+from extractor import Extractor
 # import sys
 width = 3840//4
 height = 2160//4
 
 display = Display(width, height)
-feature_extractor = FeatureExtractor()
+F = 1
+K = np.array(([F, 0, width//2], [0, F, height//2],[0, 0, 1]))
+feature_extractor = Extractor(K)
 # orb = cv2.ORB_create()
 
 def process_frame(image):
@@ -18,10 +20,15 @@ def process_frame(image):
     matches = feature_extractor.extract(image)
 
     for point1, point2 in matches:
-        x, y = map(lambda x: int(round(x)), point1)
-        match_x, match_y = map(lambda x: int(round(x)), point2)
+        # x, y = map(lambda x: int(round(x)), point1)
+        # match_x, match_y = map(lambda x: int(round(x)), point2)
+        
+        x, y = feature_extractor.denormalize(point1)
+        match_x, match_y = feature_extractor.denormalize(point2)
+
         cv2.circle(image, (x,y), color=(0, 255, 0), radius=3)
         cv2.line(image, (x, y), (match_x, match_y), color=(255, 0, 0))
+
     display.paint(image)
     # cv2.imshow('image', image)
     # print(image)
